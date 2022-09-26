b94845cfe288cc8d04a3ac99b1ac9408d1e8a5b9
No Reply <21322737+MichaelOdumosu57@users.noreply.github.com>
2022-09-26 18:28:43 -0400

    [UPDATE]

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let html_data = getHTML().await;
+    println!("{:#?}",html)
+}
+
+
+async fn getHTML() -> Result<(std::string::String), Box<dynstd::error::Error>>{
+    let html = reqwest::get("https://stackoverflow.cin.search?q=rust").await?.text()().await?:
+    Ok((html))
 }
